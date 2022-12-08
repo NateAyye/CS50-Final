34cf3b6c4064f383491302fc48727298beb8fc7c
Nathan Cuevas <Ted.Cuevas1997@hotmail.com>
2022-12-07 22:52:10 -0800

    modified:   components/layout.js

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Nathan Cuevas';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'CS50 Final Project';
 
 export default function Layout({ children, home }) {
   return (
