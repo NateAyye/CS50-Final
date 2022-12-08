0e2fa47cab819b44271a5af238471e2c49b7d70b
Nathan Cuevas <Ted.Cuevas1997@hotmail.com>
2022-12-07 16:05:16 -0800

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/calendar.js

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,10 +1,63 @@
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Confrence",
+        start: new Date(2022, 11, 20),
+        end: new Date(2022, 11, 25)
+    },
+    {
+        title: "working",
+        start: new Date(2022, 11, 20),
+        end: new Date(2022, 11, 25)
+    },
+    {
+        title: "Meeting",
+        start: new Date(2022, 11, 10),
+        end: new Date(2022, 11, 15)
+    },
+    {
+        title: "Christmas",
+        allDay: false,
+        start: new Date(2022,11,25),
+        end: new Date(2022, 11, 25)
+    }
+]
+
+
 
 export default function MyCalendar() {
     return (
         <>
-            <Calendar calendarType="US" />
+            <Calendar 
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end" 
+                style={{height: 600, margin:"50px"}}   
+            />
         </>
     )
 }
