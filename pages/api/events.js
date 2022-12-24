f16ee76577340ecf61a5f64ae0b48851804ed041
Nathan a cuevas <111668194+NateAyye@users.noreply.github.com>
2022-12-24 02:50:27 -0800

    modified:   pages/api/events.js

--- pages/api/events.js
+++ pages/api/events.js
@@ -3,14 +3,18 @@ import prisma from "/lib/prisma";
 export default async function handler(req, res) {
   if (req.method == "POST") {
     const data = await req.body;
-    console.log(data.body.user.id);
+    console.log(data.body.user.email);
 
 
     const events = await prisma.events.findMany({
       where: {
-          userId: 1,
+          users: {
+            email: data.body.user.email,
+          },
       }
     })
+    console.log(events);
+
     res.status(200).send({ data: events });
   } else {
     res.status(404).json({ message: "Hello to the post message" });
